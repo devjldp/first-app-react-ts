1fbb38b2c0e0100673ee709d531366ca700da53e
JoseDev <joseluisdp297@gmail.com>
2024-07-19 17:06:48 +0100

    Update Todos

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,4 +1,15 @@
-export const Todos: React.FC = ({ todos }) => {
+import { type ListOfTodos } from "../types"
+
+
+// Define the Props interface for the Todos component
+interface Props {
+    todos: ListOfTodos // The component expects a prop 'todos', which is a list of Todo items
+}
+// type is used to define a type and can't be easily extended.
+
+// Define the Todos functional component, which accepts Props
+export const Todos: React.FC<Props> = ({ todos }) => {
+    // FC es un component, <Props> es de tipo generico cada persona puede pasar unsas props diferentes.
     return (
         <ul>
             {todos.map(todo => {
