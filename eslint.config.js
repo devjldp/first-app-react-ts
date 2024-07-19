d8bb3b2a527bd7e992bfc413031c2f11dfc844ec
JoseDev <joseluisdp297@gmail.com>
2024-07-19 15:54:06 +0100

    Update configurations

---
+++ eslint.config.js
@@ -0,0 +1,35 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]
+  },
+  {
+    languageOptions: {
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    }
+  },
+  {
+    languageOptions: {
+      globals: globals.browser
+    }
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    plugins: ['react', 'react-refresh'],
+    rules: {
+      'react/react-in-jsx-scope': 'off', // Disable rule for React 17+
+      // Add other rules or customizations here
+    },
+  },
+  pluginReactConfig,
+];
