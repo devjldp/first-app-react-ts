672153c5fcaa14a73f865c0fcbaf7db574d0f4f4
JoseDev <joseluisdp297@gmail.com>
2024-07-20 07:50:13 +0100

    Add a new logic for onchange

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -10,6 +10,21 @@ interface Props extends TodoType {
 export const Todo: React.FC<Props> = ({ id, title, completed, onRemoveTodo, onToggleCompleted }) => {
     // console.log(completed)
 
+    /* Diferent way to manage the checkbox
+    const handleChangeCheckbox = (event: React.ChangeEvent<HTMLInputElement>): void => {
+        onToggleCompleted({
+            id, completed: event.target.checked
+        })
+    }
+        And then in onChange we can use handleChangeCheckbox directly.
+
+        * 'event' is a parameter of the 'handleChangeCheckbox' function.
+        * Its type is 'React.ChangeEvent<HTMLInputElement>' which means:
+        * 'React.ChangeEvent' is a type that represents a change event in React.
+        * 'HTMLInputElement' specifies that the event is coming from an input element of type checkbox (or any input element).
+        * The ': void' after the arrow function indicates that 'handleChangeCheckbox' does not return any value.
+    */
+
     return (
         <div className="list-element">
             <li className={`${completed ? 'completed' : ''}`}>{title}</li>
